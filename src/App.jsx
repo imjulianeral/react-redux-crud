bbd586dbae13cc3e7197e3cc577e912be7806132
Jorge <julianacero98@gmail.com>
2019-05-02 14:37:22 -0500

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Products from './components/Products';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
-import Header from './components/Header';
+import addProduct from './components/addProduct';
+import updateProduct from './components/updateProduct';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={ store }>
-        <Fragment>
-          <Header/>
-        </Fragment>
+        <Router>
+          <Fragment>
+            <Header/>
+
+            <div className="containter">
+              <Switch>
+                <Route exact path="/" component={Products}/>
+                <Route exact path="/products/add" component={addProduct}/>
+                <Route exact path="/products/update/:id" component={updateProduct}/>
+              </Switch>
+            </div>
+
+          </Fragment>
+        </Router>
       </Provider>
     )
   }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between d-flex">
-        <h1>CRUD - React, Redux, REST API & Axios</h1>
-        <button type="button" className="btn btn-danger my-2 my-sm-0 nuevo-post">Add Product &#43;</button>
+        <h1><Link to={'/'} className="text-light">CRUD - React, Redux, REST API & Axios</Link></h1>
+        <Link to={'/products/add'} className="btn btn-danger nuevo-post">Add Product &#43;</Link>
       </nav>
     )
   }
