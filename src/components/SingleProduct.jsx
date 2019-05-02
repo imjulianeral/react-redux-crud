c4d7258360a066330d2a495e38325867c81d246c
Jorge <julianacero98@gmail.com>
2019-05-02 15:49:59 -0500

    SingleProduct interface completed

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class SingleProduct extends Component {
   render() {
@@ -7,11 +8,12 @@ export default class SingleProduct extends Component {
       <li className="list-group-item">
         <div className="row justify-content-between align-items-center">
           <div className="col-md-8 d-flex justify-content-between align-items-center">
-            <p className="text-dark">{ nombre }</p>
+            <p className="text-dark m-0">{ nombre }</p>
             <span className="badge badge-warning text-dark">$ { precio }</span>
           </div>
-          <div className="col-md-4">
-            2
+          <div className="col-md-4 d-flex justify-content-end acciones">
+            <Link to={`/products/update/${id}`} className="btn btn-primary mr-2">Edit</Link>
+            <button type="button" className="btn btn-danger">Delete</button>
           </div>
         </div>
       </li>
