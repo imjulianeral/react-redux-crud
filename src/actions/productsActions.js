9daaa036b867b65c60d31e57c02c8298f5e8def3
Jorge <julianacero98@gmail.com>
2019-05-02 23:40:37 -0500

    README added

--- src/actions/productsActions.js
+++ src/actions/productsActions.js
@@ -2,35 +2,35 @@ import { SHOW_PRODUCTS, DELETE_PRODUCT, ADD_PRODUCT, SHOW_SINGLE_PRODUCT, UPDATE
 import axios from 'axios';
 
 export const showProducts = () => async dispatch => {
-    const resp = await axios.get('http://localhost:5000/products');
+    const resp = await axios.get('https://my-json-server.typicode.com/GeorgeSteel/JSON-Server-products/products');
     dispatch({
         type: SHOW_PRODUCTS,
         payload: resp.data
     });
 }
 export const showSingleProduct = id => async dispatch => {
-    const resp = await axios.get(`http://localhost:5000/products/${id}`);
+    const resp = await axios.get(`https://my-json-server.typicode.com/GeorgeSteel/JSON-Server-products/products/${id}`);
     dispatch({
         type: SHOW_SINGLE_PRODUCT,
         payload: resp.data
     });
 }
 export const deleteProducts = id => async dispatch => {
-    await axios.delete(`http://localhost:5000/products/${id}`);
+    await axios.delete(`https://my-json-server.typicode.com/GeorgeSteel/JSON-Server-products/products/${id}`);
     dispatch({
         type: DELETE_PRODUCT,
         payload: id
     });
 }
 export const addProduct = post => async dispatch => {
-    const resp = await axios.post(`http://localhost:5000/products/`, post);
+    const resp = await axios.post(`https://my-json-server.typicode.com/GeorgeSteel/JSON-Server-products/products/`, post);
     dispatch({
         type: ADD_PRODUCT,
         payload: resp.data
     });
 }
 export const updateProduct = product => async dispatch => {
-    const resp = await axios.put(`http://localhost:5000/products/${product.id}`, product);
+    const resp = await axios.put(`https://my-json-server.typicode.com/GeorgeSteel/JSON-Server-products/products/${product.id}`, product);
     dispatch({
         type: UPDATE_PRODUCT,
         payload: resp.data
